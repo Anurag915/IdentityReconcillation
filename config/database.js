9961fb1469f290f0fb67cf05409d34f4f0d99518
ANURAG PRAJAPATI <anuragprajapati02005@gmail.com>
2025-02-14 19:29:58 +0530

    changed the host

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: "127.0.0.1",
+    host: "mysql-154e5ccd-anuragprajapati02005-7654.h.aivencloud.com",
     dialect: "mysql",
     logging: false,
 });
